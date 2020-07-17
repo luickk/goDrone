8453c6a28ed81ec12acfb913d1629770df6de834
luick klippel <gelberdev@gmail.com>
2020-07-17 18:52:24 +0200

    adding handlers to web interface

--- userInterface/webInterface/webInterface.go
+++ userInterface/webInterface/webInterface.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 	utils "goDrone/utils/utils"
 	"net/http"
+	"net/url"
 	rcfNodeClient "rcf/rcfNodeClient"
 	"strconv"
 )
+
+var ccConnected, gpsConnected bool
+var gpsClient, ccClient rcfNodeClient.Client
 
 // defines wether client stores states and blocks possibly dangerous service/action executions
 var STATELESS bool
@@ -21,8 +25,8 @@ func main() {
 
 	airborne = false
 
-	ccClient, ccConnected := rcfNodeClient.NodeOpenConn(1050)
-	gpsClient, gpsConnected := rcfNodeClient.NodeOpenConn(1051)
+	ccClient, ccConnected = rcfNodeClient.NodeOpenConn(1050)
+	gpsClient, gpsConnected = rcfNodeClient.NodeOpenConn(1051)
 
 	if !ccConnected {
 		println("cc conn failed")
@@ -49,34 +53,39 @@ func main() {
 }
 
 func reconnectHandler(w http.ResponseWriter, r *http.Request) {
-
-	if string(args[0]) == "reconnect" {
-		if len(args) == 2 {
-			if args[1] == "gps" {
-				gpsClient, gpsConnected = rcfNodeClient.NodeOpenConn(1051)
-			} else if args[1] == "cc" {
-				ccClient, ccConnected = rcfNodeClient.NodeOpenConn(1050)
-			}
-			if !ccConnected {
-				println("cc conn failed")
-			} else if !gpsConnected {
-				println("gps conn failed")
-			}
-		}
+	gpsClient, gpsConnected = rcfNodeClient.NodeOpenConn(1051)
+	ccClient, ccConnected = rcfNodeClient.NodeOpenConn(1050)
+	if !ccConnected {
+		println("cc conn failed")
+		w.Write([]byte("could not connect to cc Node"))
+	} else if !gpsConnected {
+		println("gps conn failed")
+		w.Write([]byte("could not connect to gps Node"))
 	}
 }
 func takeOffHandler(w http.ResponseWriter, r *http.Request) {
-	intAlt, err := strconv.Atoi(args[1])
-	if err == nil {
-		if !airborne && !STATELESS {
-			result := rcfNodeClient.ServiceExec(ccClient, "takeoff", utils.IntToByteArray(int64(intAlt)))
-			airborne = true
-			fmt.Println(string(result))
-		} else {
-			println("can not take of if airborne")
-		}
+	parsedURL, err := url.Parse(r.URL.String())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
+		return
+	}
+
+	params := parsedURL.Query()
+	intAlt, err := strconv.Atoi(params.Get("alt"))
+
+	if err != nil {
+		w.Write([]byte("take off alt conv err"))
+		println("take off alt conv err")
+		return
+	}
+	if !airborne && !STATELESS {
+		result := rcfNodeClient.ServiceExec(ccClient, "takeoff", utils.IntToByteArray(int64(intAlt)))
+		airborne = true
+		fmt.Println(string(result))
 	} else {
-		println("takoff alt conv error")
+		println("can not take of if airborne")
+		w.Write([]byte("can not take of if airborne"))
 	}
 }
 
@@ -85,6 +94,7 @@ func landHandler(w http.ResponseWriter, r *http.Request) {
 		rcfNodeClient.ActionExec(ccClient, "land", []byte(""))
 	} else {
 		println("can only land if airborne")
+		w.Write([]byte("can only land if airborne"))
 		airborne = false
 	}
 }
@@ -92,87 +102,121 @@ func landHandler(w http.ResponseWriter, r *http.Request) {
 func markHomePos(w http.ResponseWriter, r *http.Request) {
 	if !airborne && !STATELESS {
 		rcfNodeClient.ActionExec(ccClient, "markhomepos", []byte(""))
+		w.Write([]byte("set home pos"))
 	} else {
 		println("cannot set home pos if airborne")
+		w.Write([]byte("cannot set gome pos if airborne"))
 	}
 }
 
 func turntoHandler(w http.ResponseWriter, r *http.Request) {
-	if len(args) == 2 {
-		intAlt, err := strconv.Atoi(args[1])
-		if err == nil {
-			if airborne && !STATELESS {
-				result := rcfNodeClient.ServiceExec(ccClient, "turnto", utils.IntToByteArray(int64(intAlt)))
-				fmt.Println(string(result))
-			} else {
-				println("can only rotate drone if airbrone")
-			}
+	parsedURL, err := url.Parse(r.URL.String())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
+		return
+	}
+
+	params := parsedURL.Query()
+	dir, err := strconv.Atoi(params.Get("dir"))
+
+	if err != nil {
+		println("missing arg deg for service turnto")
+		w.Write([]byte("missing arg deg for service turnto"))
+		return
+	}
+
+	if err == nil {
+		if airborne && !STATELESS {
+			result := rcfNodeClient.ServiceExec(ccClient, "turnto", utils.IntToByteArray(int64(dir)))
+			fmt.Println(string(result))
 		} else {
-			println("turnto deg conv error")
+			println("can only rotate drone if airbrone")
+			w.Write([]byte("can only rotate drone if airbrone"))
 		}
 	} else {
-		println("missing arg deg for service turnto")
+		println("turnto deg conv error")
+		w.Write([]byte("turnto deg conv error"))
 	}
 
 }
 
 func flytolatlonHandler(w http.ResponseWriter, r *http.Request) {
-	if len(args) == 3 {
-		lat, latErr := strconv.ParseFloat(args[1], 64)
-		lon, lonErr := strconv.ParseFloat(args[2], 64)
-		if latErr == nil && lonErr == nil {
-			if airborne && !STATELESS {
-				result := rcfNodeClient.ServiceExec(ccClient, "flytolatlon", utils.EncodeLatLonAlt(lat, lon, 0))
-				fmt.Println(string(result))
-			} else {
-				println("can only change location if airborne")
-			}
-		} else {
-			println("takoff alt conv error")
-		}
+	parsedURL, err := url.Parse(r.URL.String())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
+		return
+	}
+
+	params := parsedURL.Query()
+	lat, errLon := strconv.ParseFloat(params.Get("lat"), 64)
+	lon, errLat := strconv.ParseFloat(params.Get("lon"), 64)
+
+	if errLat == nil || errLon == nil {
+		println("missing arg for service flyToLatLon")
+		w.Write([]byte("missing arg for service flyToLatLon"))
+		return
+	}
+
+	if airborne && !STATELESS {
+		result := rcfNodeClient.ServiceExec(ccClient, "flytolatlon", utils.EncodeLatLonAlt(lat, lon, 0))
+		fmt.Println(string(result))
 	} else {
-		println("missing arfs lat lon for service flytolatlon")
+		println("can only change location if airborne")
 	}
 }
 
 func listTopicsHandler(w http.ResponseWriter, r *http.Request) {
-	if len(args) >= 2 {
-		data_map := make(map[string]string)
-		data_map["cli"] = args[2]
-		rcfNodeClient.TopicPublishGlobData(ccClient, args[1], data_map)
-	}
+	// data_map := make(map[string]string)
+	// data_map["cli"] = args[2]
+	// rcfNodeClient.TopicPublishGlobData(ccClient, args[1], data_map)
+
 }
 func setNeutralHandler(w http.ResponseWriter, r *http.Request) {
 	if !airborne && !STATELESS {
 		rcfNodeClient.ActionExec(ccClient, "setneutral", []byte(""))
+		w.Write([]byte("set stick pos to neutral"))
 	} else {
-		println("can only set to neutral if on ground")
+		println("can only set to neutral if not airborne")
+		w.Write([]byte("can only set to neutral if not airborne"))
 	}
 }
 func setStateHandler(w http.ResponseWriter, r *http.Request) {
-	if len(args) >= 2 {
-		if args[1] == "airborne" {
-			if args[2] == "true" {
-				airborne = true
-			} else if args[2] == "false" {
-				airborne = false
-			}
+	parsedURL, err := url.Parse(r.URL.String())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
+		return
+	}
+
+	params := parsedURL.Query()
+	state := params.Get("state")
+	val := params.Get("state")
+
+	if state == "airborne" {
+		if val == "true" {
+			airborne = true
+		} else if val == "false" {
+			airborne = false
 		}
 	}
 }
 func listStatesHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("ariborne: ", airborne)
+	w.Write([]byte("ariborne: " + strconv.FormatBool(airborne)))
+
 	fmt.Println("gpsConnected: ", gpsConnected)
+	w.Write([]byte("gpsConnected: " + strconv.FormatBool(gpsConnected)))
+
 	fmt.Println("ccConnected: ", ccConnected)
+	w.Write([]byte("ccConnected: " + strconv.FormatBool(ccConnected)))
 }
 func getGpsPosHandler(w http.ResponseWriter, r *http.Request) {
 	elements := rcfNodeClient.TopicPullGlobData(gpsClient, 1, "gpsData")
 	fmt.Println(elements)
 }
 func endcomHandler(w http.ResponseWriter, r *http.Request) {
-	if len(args) >= 0 {
-		rcfNodeClient.NodeCloseConn(ccClient)
-		rcfNodeClient.NodeCloseConn(gpsClient)
-		return
-	}
+	rcfNodeClient.NodeCloseConn(ccClient)
+	rcfNodeClient.NodeCloseConn(gpsClient)
 }
